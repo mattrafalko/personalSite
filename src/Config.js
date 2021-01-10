3f45d0e90bae78f0ee6685f947f8526d48b936f9
mattrafalko <mattrafalko@gmail.com>
2021-01-10 13:39:51 -0600

    extract userData to config file

---
+++ src/Config.js
@@ -0,0 +1,45 @@
+export const userData = {
+  links: {
+    linkedIn: 'https://www.linkedin.com/in/mrafalko',
+    email: 'mattrafalko@gmail.com',
+  },
+  aboutMe: {
+    description:
+      'A software developer with a passion for anything front-end.\nLets work together.',
+    imgAlt: 'My dog Apollo and I',
+  },
+  resumeData: [
+    {
+      company: 'Uline',
+      title: 'Software Developer',
+      dates: {
+        start: '2/2020',
+        end: 'Current',
+      },
+    },
+    {
+      company: 'Milliman IntelliScript',
+      title: 'Client Integration Engineer',
+      dates: {
+        start: '2/2018',
+        end: '2/2020',
+      },
+    },
+    {
+      company: 'OmniVista Solutions',
+      title: 'System Analyst - Intern',
+      dates: {
+        start: '1/2017',
+        end: '5/2017',
+      },
+    },
+    {
+      company: 'Iowa State Foundation',
+      title: 'Program Assistant',
+      dates: {
+        start: '9/2013',
+        end: '8/2016',
+      },
+    },
+  ],
+};
