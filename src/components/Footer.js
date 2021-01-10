e94c29fe05f0fdd94a6ed2a94084e1b1b83fb2ee
mattrafalko <mattrafalko@gmail.com>
2021-01-10 13:31:41 -0600

    build footer icons programmatically

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,32 +6,30 @@ import Linkedin from '../assets/icons/linkedin.svg';
 import { motion } from 'framer-motion';
 import { GithubContext } from '../context/GitHubContext';
 import { userData } from '../context/UserData';
-const Footer = () => {
-  const { githubUserInfo } = useContext(GithubContext);
 
-  const { html_url } = githubUserInfo;
+const Footer = () => {
+  const icons = { email: Email, linkedIn: Linkedin, github: Github };
+
+  const { githubUserInfo } = useContext(GithubContext);
+  const links = { ...userData.links, github: githubUserInfo?.html_url };
+
+  const items = Object.keys(icons).map((x) => {
+    return { link: links[x], icon: icons[x] };
+  });
+
+  const footerIcons = items.map((item, i) => (
+    <motion.div whileHover={{ scale: 1.2 }} key={i}>
+      <a href={`mailto:${item.link}`}>
+        <ReactSVG src={item.icon} />
+      </a>
+    </motion.div>
+  ));
 
   return (
     <React.Fragment>
       <div className='bg-gray-900 fixed bottom-0 px-5 py-4 w-full flex border-t-2 border-green-500'>
         <div className='max-w-2xl w-full flex justify-between mx-auto pb-1'>
-          <motion.div className='' whileHover={{ scale: 1.2 }}>
-            <a href={`mailto:${userData.links.email}`}>
-              <ReactSVG src={Email} />
-            </a>
-          </motion.div>
-          {githubUserInfo ? (
-            <motion.div className='' whileHover={{ scale: 1.2 }}>
-              <a href={html_url} target='_blank' rel='noreferrer'>
-                <ReactSVG src={Github} />
-              </a>
-            </motion.div>
-          ) : null}
-          <motion.div className='' whileHover={{ scale: 1.2 }}>
-            <a href={userData.links.linkedIn} target='_blank' rel='noreferrer'>
-              <ReactSVG src={Linkedin} />
-            </a>
-          </motion.div>
+          {footerIcons}
         </div>
       </div>
     </React.Fragment>
