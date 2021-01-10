579e816289a937d42b88445b4b3c1cd4dffd9b1b
mattrafalko <mattrafalko@gmail.com>
2021-01-10 11:58:07 -0600

    added context files

---
+++ src/context/UserData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const getGithubUserData = async () => {
+  return await axios.get(
+    `https://api.github.com/users/mattrafalko?client_id=${process.env.REACT_APP_GITHUB_CLIENTID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENTSECRET}`
+  );
+};
+
+export const getGithubProjectdata = async () => {
+  const projects = await axios.get(
+    `https://api.github.com/users/mattrafalko/repos?per_page=50&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENTID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENTSECRET}`
+  );
+  let filtered = projects.data.filter((x) => x.fork === false);
+  return filtered;
+};
+
+export const userData = {
+  links: {
+    linkedIn: 'https://www.linkedin.com/in/mrafalko',
+    email: 'mattrafalko@gmail.com',
+  },
+};
